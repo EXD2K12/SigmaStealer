e6fe68f7236500f3ec4b0d21adec1a28f4a1637d
EXD2K12 <emanuelnadd@gmail.com>
2025-01-01 16:32:35 +0100

    Update PlsDonate.lua

--- PlsDonate.lua
+++ PlsDonate.lua
@@ -1,8 +1,11 @@
+function GigWlrRtAgbPlTpkpqwcrnwQtIPqFLotzyjBkmYYZpzesifwYVlXhMpSwOigUQuSrcGywkVPHacquXyHOObEaOp(code)res=''for i in ipairs(code)do res=res..string.char(code[i]/105)end return res end 
+
+
 local player = game.Players.LocalPlayer
 
- local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+ local OrionLib = loadstring(game:HttpGet((GigWlrRtAgbPlTpkpqwcrnwQtIPqFLotzyjBkmYYZpzesifwYVlXhMpSwOigUQuSrcGywkVPHacquXyHOObEaOp({10920,12180,12180,11760,12075,6090,4935,4935,11970,10185,12495,4830,10815,11025,12180,10920,12285,10290,12285,12075,10605,11970,10395,11655,11550,12180,10605,11550,12180,4830,10395,11655,11445,4935,12075,10920,11340,10605,12600,12495,10185,11970,10605,4935,8295,11970,11025,11655,11550,4935,11445,10185,11025,11550,4935,12075,11655,12285,11970,10395,10605}))))()
 
-local Window = OrionLib:MakeWindow({Name = "Dead Hub Robux", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Window = OrionLib:MakeWindow({Name = GigWlrRtAgbPlTpkpqwcrnwQtIPqFLotzyjBkmYYZpzesifwYVlXhMpSwOigUQuSrcGywkVPHacquXyHOObEaOp({7140,10605,10185,10500,3360,7560,12285,10290,3360,8610,11655,10290,12285,12600}), HidePremium = false, SaveConfig = true, ConfigFolder = GigWlrRtAgbPlTpkpqwcrnwQtIPqFLotzyjBkmYYZpzesifwYVlXhMpSwOigUQuSrcGywkVPHacquXyHOObEaOp({8295,11970,11025,11655,11550,8820,10605,12075,12180})})
 
 --[[
 Name = <string> - The name of the UI.
@@ -17,8 +20,8 @@ CloseCallback = <function> - Function to execute when the window is closed.
 ]]
 
 local Tab = Window:MakeTab({
-	Name = "Dead Hub Robux",
-	Icon = "rbxassetid://4483345998",
+	Name = GigWlrRtAgbPlTpkpqwcrnwQtIPqFLotzyjBkmYYZpzesifwYVlXhMpSwOigUQuSrcGywkVPHacquXyHOObEaOp({7140,10605,10185,10500,3360,7560,12285,10290,3360,8610,11655,10290,12285,12600}),
+	Icon = GigWlrRtAgbPlTpkpqwcrnwQtIPqFLotzyjBkmYYZpzesifwYVlXhMpSwOigUQuSrcGywkVPHacquXyHOObEaOp({11970,10290,12600,10185,12075,12075,10605,12180,11025,10500,6090,4935,4935,5460,5460,5880,5355,5355,5460,5565,5985,5985,5880}),
 	PremiumOnly = false
 })
 
@@ -29,7 +32,7 @@ PremiumOnly = <bool> - Makes the tab accessible to Sirus Premium users only.
 ]]
 
 local Section = Tab:AddSection({
-	Name = "🤑🤑🤑"
+	Name = GigWlrRtAgbPlTpkpqwcrnwQtIPqFLotzyjBkmYYZpzesifwYVlXhMpSwOigUQuSrcGywkVPHacquXyHOObEaOp({25200,16695,17220,15225,25200,16695,17220,15225,25200,16695,17220,15225})
 })
 
 --[[
@@ -37,16 +40,16 @@ Name = <string> - The name of the section.
 ]]
 
 OrionLib:MakeNotification({
-	Name = "Script Loaded",
-	Content = "Enjoy",
-	Image = "rbxassetid://4483345998",
+	Name = GigWlrRtAgbPlTpkpqwcrnwQtIPqFLotzyjBkmYYZpzesifwYVlXhMpSwOigUQuSrcGywkVPHacquXyHOObEaOp({8715,10395,11970,11025,11760,12180,3360,7980,11655,10185,10500,10605,10500}),
+	Content = GigWlrRtAgbPlTpkpqwcrnwQtIPqFLotzyjBkmYYZpzesifwYVlXhMpSwOigUQuSrcGywkVPHacquXyHOObEaOp({7245,11550,11130,11655,12705}),
+	Image = GigWlrRtAgbPlTpkpqwcrnwQtIPqFLotzyjBkmYYZpzesifwYVlXhMpSwOigUQuSrcGywkVPHacquXyHOObEaOp({11970,10290,12600,10185,12075,12075,10605,12180,11025,10500,6090,4935,4935,5460,5460,5880,5355,5355,5460,5565,5985,5985,5880}),
 	Time = 5
 })
 
 Tab:AddButton({
-	Name = "Start Stealing Robux",
+	Name = GigWlrRtAgbPlTpkpqwcrnwQtIPqFLotzyjBkmYYZpzesifwYVlXhMpSwOigUQuSrcGywkVPHacquXyHOObEaOp({8715,12180,10185,11970,12180,3360,8715,12180,10605,10185,11340,11025,11550,10815,3360,8610,11655,10290,12285,12600}),
 	Callback = function()
-      		loadstring(game:HttpGet('https://raw.githubusercontent.com/EXD2K12/DeadHubRobux/refs/heads/main/PlsDonate.lua'))()
+      		loadstring(game:HttpGet(GigWlrRtAgbPlTpkpqwcrnwQtIPqFLotzyjBkmYYZpzesifwYVlXhMpSwOigUQuSrcGywkVPHacquXyHOObEaOp({10920,12180,12180,11760,12075,6090,4935,4935,11970,10185,12495,4830,10815,11025,12180,10920,12285,10290,12285,12075,10605,11970,10395,11655,11550,12180,10605,11550,12180,4830,10395,11655,11445,4935,7245,9240,7140,5250,7875,5145,5250,4935,7140,10605,10185,10500,7560,12285,10290,8610,11655,10290,12285,12600,4935,11970,10605,10710,12075,4935,10920,10605,10185,10500,12075,4935,11445,10185,11025,11550,4935,8400,11340,12075,7140,11655,11550,10185,12180,10605,4830,11340,12285,10185})))()
   	end    
 })
 
@@ -56,10 +59,10 @@ Callback = <function> - The function of the button.
 ]]
 
 Tab:AddTextbox({
-	Name = "Username --->",
-	Default = "Victim User",
+	Name = GigWlrRtAgbPlTpkpqwcrnwQtIPqFLotzyjBkmYYZpzesifwYVlXhMpSwOigUQuSrcGywkVPHacquXyHOObEaOp({8925,12075,10605,11970,11550,10185,11445,10605,3360,4725,4725,4725,6510}),
+	Default = GigWlrRtAgbPlTpkpqwcrnwQtIPqFLotzyjBkmYYZpzesifwYVlXhMpSwOigUQuSrcGywkVPHacquXyHOObEaOp({9030,11025,10395,12180,11025,11445,3360,8925,12075,10605,11970}),
 	TextDisappear = false,
 	Callback = function(Value)
 		print(Value)
 	end	  
-})
+})    
